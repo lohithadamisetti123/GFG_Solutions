ab7b8b49afc630ddc8cb639c59a2a31bf8a65ac0
LOHITHA DAMISETTI <23mh1a4413@acoe.edu.in>
2025-09-10 21:10:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -0,0 +1,22 @@
+/*The structure of the class is
+class SortedStack{
+public:
+    stack<int> s;
+    void sort();
+};
+*/
+
+/* The below method sorts the stack s
+you are required to complete the below method */
+void SortedStack ::sort() {
+    // Your code here
+     vector<int> v;
+      while(!s.empty()){
+         v.push_back(s.top());
+         s.pop();
+      }
+      std::sort(v.begin(),v.end());
+      for(int i = 0;i<v.size();i++){
+          s.push(v[i]);
+      }
+}
